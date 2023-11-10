30213032183aee25dd9ff16996e2085966b96546
Hajar <hajarsaidi32@gmail.com>
2023-11-10 15:43:47 +0100

    i just want to save this version

--- src/components/Messages/index.jsx
+++ src/components/Messages/index.jsx
@@ -1,9 +1,31 @@
 
+// Messages.jsx
+import React from 'react';
+import ChatList from './ChatList';
+import ChatWindow from './ChatWindow';
+import style from '/home/hajar/Desktop/front/src/style.js';
+import { useState, useEffect } from "react";
+
+// const Messages = () => {
+//       return (
+//       <div className={`flex ${style.boxWidth} ${style.rounded} ${style.blueBlur} overflow-hidden`}>
+//         <div className={`flex-none ${style.chatListWidth}`}> {/* Control the width in your style.js */}
+//           <ChatList />
+//         </div>
+//         <div className={`flex-grow ${style.chatWindowFlex}`}>
+//           <ChatWindow />
+//         </div>
+//       </div>
+//     );
+//   };
+
 
 const Messages = () => {
   return (
-    <div>Messages</div>
-  )
-}
-
-export default Messages
+    <div className={`flex ${style.boxWidth} ${style.rounded} ${style.blueBlur} overflow-hidden`}>
+      <ChatList />
+      <ChatWindow />
+    </div>
+  );
+};
+  export default Messages;
