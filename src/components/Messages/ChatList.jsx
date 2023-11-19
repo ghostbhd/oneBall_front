78f930b93042b202b29dbef5fbd119d31c2a5942
Hajar <hajarsaidi32@gmail.com>
2023-11-19 23:41:38 +0100

    start mergin with the backend  using websockets

--- src/components/Messages/ChatList.jsx
+++ src/components/Messages/ChatList.jsx
@@ -1,16 +1,54 @@
 import React from 'react';
 import style from "../../style";
-import  { useState } from 'react';
+import  { useState, useEffect } from 'react';
 import SearchBar from '/home/hajar/Desktop/front/src/components/Messages/searchBar.jsx';
+import io from 'socket.io-client';
 
 
-import { mockChats } from '/home/hajar/Desktop/front/src/data/mockChats.js';
+console.log("-----------1-------------------");
+
+const ChatList = ({ activeChat, setActiveChat, onSearch }) => {
+  const [chats, setChats] = useState([]);
+
+  useEffect(() => {
+    const socket = io('http://localhost:3009');
+
+    socket.on('connection', data => {
+      console.log(data);
+    });
+
+    socket.on('latest-messages', updatedChats => {
+      setChats(updatedChats);
+    });
+
+    socket.on('new-message', newMessage => {
+      setChats((prevChats) => {
+        const chatToUpdateIndex = prevChats.findIndex(chat => chat.id === newMessage.chatId);
+        if (chatToUpdateIndex >= 0) {
+          const updatedChats = [...prevChats];
+          updatedChats[chatToUpdateIndex].lastMessage = newMessage.content;
+          return updatedChats;
+        }
+        return prevChats;
+      });
+    });
+
+    // Emit a request for the latest messages
+    socket.emit('request-latest-messages',2); // i need to Replace with actual user ID
+  
+    return () => {
+      socket.off('connection');
+      socket.off('latest-messages');
+      socket.off('new-message');
+      socket.disconnect();
+      
+    };
+  }, []);
+  
 
 
-const ChatList = ({ activeChat, setActiveChat ,onSearch}) => {
   const handleChatClick = (chatId) => {
     setActiveChat(chatId);
-
   };
 
   return (
@@ -19,7 +57,7 @@ const ChatList = ({ activeChat, setActiveChat ,onSearch}) => {
         <SearchBar onSearch={onSearch} />
       </div>
       <div className="h-5/5 rounded-b-2xl overflow-y-auto">
-      {mockChats.map((chat) => (
+      {chats.map((chat) => (
   <div
     key={chat.id}
     className={`flex items-center p-2 ${style.transition} hover:bg-opacity-70 ${activeChat === chat.id ? style.activeChatItem : ''}`}
@@ -28,13 +66,13 @@ const ChatList = ({ activeChat, setActiveChat ,onSearch}) => {
     {/*! Wrapper div with relative positioning */}
     <div className="relative">
       {/* Status indicator with absolute to place it at the bottom-right corner of the avatar image.*/}
-      <span className={`absolute w-12 h-12 rounded-full border-[3px]   ${chat.status === 'online' ? style.online : chat.status === 'offline' ? style.offline : style.inGame}`}>
-      </span>
-      <img className="w-12 h-12 rounded-full " src={chat.avatar} alt={`${chat.name}`} />
+       <span className={`absolute w-12 h-12 rounded-full border-[3px]   ${chat.status === 'online' ? style.online : chat.status === 'offline' ? style.offline : style.inGame}`}>
+      </span>  
+      <img className="w-12 h-12 rounded-full " src="https://i.pinimg.com/236x/7f/61/ef/7f61efa1cfbf210ac8df7a813cf56a1e.jpg" alt={`${chat.name}`} />
     </div>
     <div>
       <h3 className="text-white px-3">{chat.name}</h3>
-      <p className="text-gray-400 px-3">{chat.message}</p>
+      <p className="text-gray-400 px-3">{chat.lastMessage}</p>
     </div>
   </div>
 ))}
--- src/components/Messages/index.jsx
+++ src/components/Messages/index.jsx
@@ -7,19 +7,48 @@ import style from '/home/hajar/Desktop/front/src/style.js';
 import { useState, useEffect } from "react";
 import SearchBar from '/home/hajar/Desktop/front/src/components/Messages/searchBar.jsx';
 import { mockChats } from '/home/hajar/Desktop/front/src/data/mockChats.js';
+import io from 'socket.io-client';
+
+const socket = io('http://localhost:3009');
+
+socket.on('connect', () => {
+  console.log('Connected to the server');
+});
+
+socket.on('connection', (data) => {
+  console.log(data); // 'Successfully connected to server'
+});
 
 
 const Messages = () => {
   const [activeChat, setActiveChat] = useState(null);
   const [searchTerm, setSearchTerm] = useState('');
+   const [latestMessages, setLatestMessages] = useState([]);
+
+   useEffect(() => {
+    socket.on('connect', () => {
+    //   // console.log('Connected to the server');
+    //   // Request latest messages for the user
+    //   socket.emit('request-latest-messages', 2); // i need to Replace with actual user ID
+    });
+
+    socket.on('latest-messages', (messages) => {
+      setLatestMessages(messages);
+    });
+
+    return () => {
+      socket.off('connect');
+    //   socket.off('latest-messages');
+    };
+  }, []);
 
   const handleSearch = (query) => {
     setSearchTerm(query);
   };
   
-  const filteredChats = mockChats.filter((chat) =>
-  chat.name.toLowerCase().includes(searchTerm.toLowerCase())
-  
+  console.log(latestMessages);
+  const filteredChats = latestMessages.filter((chat) =>
+    chat.name && chat.name.toLowerCase().includes(searchTerm.toLowerCase())
   );
   // const handleSearchIconClick = () => {
 
