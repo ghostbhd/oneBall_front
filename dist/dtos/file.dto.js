843639506303667f9e19db325280efe2907aaa61
Hajar saidi <70338330+hajar-saidi@users.noreply.github.com>
2023-12-10 12:54:52 +0100

    Delete dist directory

--- dist/dtos/file.dto.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.fileDto = void 0;
-class fileDto {
-}
-exports.fileDto = fileDto;
-//# sourceMappingURL=file.dto.js.map
