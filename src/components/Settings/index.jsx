3893a6fe265dc984b74abe9f74930a25b85bcc81
ghostbhd <bhd.a@yandex.com>
2023-12-28 19:48:13 +0100

    anass mods

--- src/components/Settings/index.jsx
+++ src/components/Settings/index.jsx
@@ -5,7 +5,7 @@ import checkMark from "../../assets/Checkmark.gif";
 
 const SuccessCheckmark = () => {
   return (
-    <div className="animate-success-check w-full h-16 text-center">
+    <div className="animate-success-check w-full text-center">
       <img src={checkMark} alt="checkMark" className="mx-auto mb-5 max-w-full h-16"/>
       <p className="text-greenclr text-lg font-semibold mt-0 mb-0">
         2-Factor Authentication Enabled Successfully
@@ -53,22 +53,22 @@ const Settings = () => {
   };
 
   return (
-    <div className={`w-full sm:h-full sm:space-x-8 xl:space-x-20 flex items-center justify-center sm:flex-row flex-col md:pb-4 pb-16 p-4 md:px-10 sm:pt-14 pt-16`}>
+    <div className={`w-full h-full flex`}>
       {isSuccess ? (
         <SuccessCheckmark />
       ) : (
-        <div className={`sm:w-7/12 w-full h-5/6 relative overflow-y-auto ${style.blueBlur} ${style.rounded} text-center`}>
-          <p className={`text-2xl font-semibold text-bLight_4 mb-1 mt-6`}>Scan QR Code</p>
+        <div className={`sm:w-max px-20 p-6 gap-2 w-11/12 flex flex-col text-center items-center h-max m-auto relative ${style.blueBlur} ${style.rounded}`}>
+          <p className={`text-2xl font-semibold text-bLight_4`}>Scan QR Code</p>
           <p className={`text-bLight_4 text-sm leading-5`}>
             To enable 2-factor authentication, scan <br />
             this QR Code with your Google Authentication App <br />
             and enter the verification code below
           </p>
-          <img src={qrImage} alt="qr-code" className="mx-auto mb-5 mt-5 max-w-full h-auto" />
+          <img src={qrImage} alt="qr-code" className="my-5 max-w-full h-auto" />
           <p className={`text-bLight_2 text-lg leading-5`}>
             Enter Verification Code :
           </p>
-          <div className="digits-grid mt-5 flex justify-center">
+          <div className="digits-grid flex justify-center">
             {digits.map((digit, index) => (
               <div className="digit-container" key={index}>
                 <input
@@ -84,11 +84,11 @@ const Settings = () => {
             ))}
           </div>
 
-          <div className="digits-grid mt-5 flex justify-center">
-            {warning && <p className="text-err text-xs mt-14">{warning}</p>}
+          <div className="w-full flex flex-col items-center justify-center">
+            {warning && <p className="text-org_3 text-xs my-2">{warning}</p>}
 
             <button
-              className="absolute left-1/4 w-1/2 h-12 bg-gradient-to-r from-bDark_3 to-bDark_5 text-white text-sm rounded transition duration-500 hover:bg-gradient-to-r hover:from-bDark_4 hover:to-bDark_3"
+              className="w-full p-4 bg-bDark_1 rounded-xl text-white text-sm"
               onClick={handleSubmit}
             >
               Submit
