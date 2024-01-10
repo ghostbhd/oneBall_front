e9b243462888bcbb1b49abaa91ab5814c8b17d95
root <yonkotitx@gmail.com>
2024-01-10 20:24:18 +0000

    backend migrated to backend repo

--- backend/src/addfriend/dto/update-addfriend.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateAddfriendDto } from './create-addfriend.dto';
-
-export class UpdateAddfriendDto extends PartialType(CreateAddfriendDto) {
-  id: number;
-}
