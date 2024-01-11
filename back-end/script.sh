d110dfb67f94726cb55d483855646e1aba2e28ca
atouati <ayoub.toouati@gmail.com>
2024-01-11 21:24:12 +0100

    docker v

--- back-end/script.sh
+++ back-end/script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd backend
-# npm update
-# npm i -f
+npm update
+npm i -f
 npm i -g @nestjs/cli
 npm run start:dev
--- front-end/script.sh
+++ front-end/script.sh
@@ -2,6 +2,6 @@
 cd frontend
 npm install -g npm@10
 npm install -g create-vite
-# npm update
-# npm i
+npm update
+npm i
 npm run dev -- --host 0.0.0.0
