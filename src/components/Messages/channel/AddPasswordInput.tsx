87494eb9bc5a06d6b5c21a79aa56d371fe0d740e
ghostbhd <bhd.a@yandex.com>
2024-01-11 15:58:20 +0100

    v17.4 pass input error

--- src/components/Messages/channel/AddPasswordInput.tsx
+++ src/components/Messages/channel/AddPasswordInput.tsx
@@ -12,8 +12,15 @@ const AddPasswordInput = ({
 }) => {
   const [addPass, setAddPass] = useState("");
   const socket = useSocket();
+  const [passwordError, setPasswordError] = useState("");
 
   const handleAddPassword = () => {
+    if (addPass.length < 3) {
+      setPasswordError("Password must be at least 3 characters long");
+      setAddPass("");
+      return;
+    }
+
     socket.emit("addpassword", {
       channelId: activeChannel,
       newPassword: addPass,
@@ -34,14 +41,17 @@ const AddPasswordInput = ({
         className={`w-1/3 flex flex-col p-4 rounded-3xl gap-4 h-max m-auto ${style.blueBlur}`}
       >
         <p className={`text-sm text-bLight_5`}>Add password</p>
-        <input
-          type="password"
-          className={`w-full rounded-full p-2 outline-none bg-bDark_4 border-2 border-bLight_5/20 text-bLight_4 placeholder:text-bLight_5`}
-          placeholder="Enter password"
-          value={addPass}
-          onChange={(e) => setAddPass(e.target.value)}
-          onKeyPress={(e) => e.key === "Enter" && handleAddPassword}
-        />
+        <div className="w-full">
+          <input
+            type="password"
+            className={`w-full rounded-full p-2 outline-none bg-bDark_4 border-2 border-bLight_5/20 text-bLight_4 placeholder:text-bLight_5`}
+            placeholder="Enter password"
+            value={addPass}
+            onChange={(e) => setAddPass(e.target.value)}
+            onKeyPress={(e) => e.key === "Enter" && handleAddPassword}
+          />
+          <p className="text-org_1/60 text-xs px-2">{passwordError}</p>
+        </div>
 
         <button
           onClick={handleAddPassword}
